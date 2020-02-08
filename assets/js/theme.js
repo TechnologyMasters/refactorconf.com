c6c587c5c8cdb509f5fdd9362e72e16a29ae45ab
Ahmad Nassri <ahmad@ahmadnassri.com>
2020-02-08 17:26:02 -0500

    feat(ie10+): add support for Internet Explorer

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,38 +1,17 @@
 //
 //
-// background-images.js
-//
-// a javascript fallback for CSS 'object-fit' property for browsers that don't support it
-
-function objectFit () {
-  if ('objectFit' in document.documentElement.style === false) {
-    $('.bg-image').each(function attachBg () {
-      const img = $(this)
-      const src = img.attr('src')
-      const classes = img.get(0).classList
-      // Replaces the default <img.bg-image> element with a <div.bg-image>
-      // to attach background using legacy friendly CSS rules
-      img.before($(`<div class="${classes}" style="background: url(${src}); background-size: cover; background-position: 50% 50%;"></div>`))
-      // Removes original <img.bg-image> as it is no longer required
-      img.remove()
-    })
-  }
-}
-
-//
-//
 //  fade-page.js
 //
 //
 
 // Page Transition to fade out when clicking a link which has opted in using class 'fade-page'
 function pageFade () {
-  const ATTR_HREF = 'href'
-  const EVENT_CLICK = 'click'
-  const SELECTOR_FADE = 'fade-page'
-  const EFFECT_DELAY = 500
+  var ATTR_HREF = 'href'
+  var EVENT_CLICK = 'click'
+  var SELECTOR_FADE = 'fade-page'
+  var EFFECT_DELAY = 500
 
-  const fadePage = document.getElementsByClassName(SELECTOR_FADE)
+  var fadePage = document.getElementsByClassName(SELECTOR_FADE)
 
   function fadePageFunction (event) {
     if (!(event.ctrlKey ||
@@ -43,8 +22,8 @@ function pageFade () {
       event.stopPropagation()
       document.body.classList.add(SELECTOR_FADE)
 
-      const href = this.getAttribute(ATTR_HREF)
-      setTimeout(() => {
+      var href = this.getAttribute(ATTR_HREF)
+      setTimeout(function () {
         if (href !== '' && href !== '#') {
           window.location.href = href
         }
@@ -52,22 +31,22 @@ function pageFade () {
     }
   }
   // Bind click event
-  for (let i = 0; i < fadePage.length; i += 1) {
+  for (var i = 0; i < fadePage.length; i += 1) {
     fadePage[i].addEventListener(EVENT_CLICK, fadePageFunction, false)
   }
 }
 
 function isIE () {
-  const ua = window.navigator.userAgent
-  const isIE = /MSIE|Trident/.test(ua)
+  var ua = window.navigator.userAgent
+  var isIE = /MSIE|Trident/.test(ua)
   return isIE
 }
 
 function injectSVG () {
-  const { SVGInjector } = window.SVGInjector
+  var SVGInjector = window.SVGInjector.SVGInjector
 
   if (isIE()) {
-    window.addEventListener('load', () => {
+    window.addEventListener('load', function () {
       SVGInjector(document.querySelectorAll('[class][data-inject-svg]'))
     })
     SVGInjector(document.querySelectorAll('[data-inject-svg]'))
@@ -76,8 +55,14 @@ function injectSVG () {
   }
 }
 
-window.addEventListener('load', () => document.querySelector('body').classList.add('loaded'))
-window.addEventListener('load', () => AOS.init({ once: true }))
+function userAgent () {
+  console.log(navigator.userAgent)
+  document.documentElement.setAttribute('data-useragent', navigator.userAgent)
+}
+
+window.addEventListener('load', userAgent)
+window.addEventListener('load', function () { document.querySelector('body').classList.add('loaded') })
+window.addEventListener('load', function () { AOS.init({ once: true }) })
 window.addEventListener('load', pageFade)
-window.addEventListener('load', objectFit)
+// window.addEventListener('load', objectFit)
 window.addEventListener('load', injectSVG)
